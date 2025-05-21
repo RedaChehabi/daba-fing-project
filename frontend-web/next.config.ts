575d42e51e32c91bc5d93c224c01575d2a26dcc4
Reda Chehabi <chehabire.tool08@gmail.com>
2025-05-21 22:31:43 +0100

    Update next.config.ts

--- frontend-web/next.config.ts
+++ frontend-web/next.config.ts
@@ -3,9 +3,9 @@
 const isElectronBuild = process.env.BUILD_TARGET === 'electron';
 
 const nextConfig = {
-  output: 'export', // Keep this commented unless specifically building for static export only for all targets
+  //output: 'export', // Keep this commented unless specifically building for static export only for all targets
   images: {
-    unoptimized: isElectronBuild, // Conditionally unoptimize for Electron
+    unoptimized: true, // Conditionally unoptimize for Electron
   },
   experimental: {
     turbo: {
